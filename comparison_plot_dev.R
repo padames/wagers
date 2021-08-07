a24326f578382ae94490deaa7c6b39f8d694de8f
pablo <pablo.adames@ucalgary.ca>
2021-08-06 21:45:15 -0600

    Added grid and labels on each simulation line

--- comparison_plot_dev.R
+++ comparison_plot_dev.R
@@ -21,21 +21,43 @@ df <- rbind(df3, df4)
 
 png(file="simwager%02d.png", width=600, height=400)
 
+wagerLabels = c("Wager 3", "Wager 4")
+
 pp <- xyplot(runningprf~tries, data=df,
        groups = wager,
-       scales=list(cex=.8, col="black", x = list(log = T), y = list(log = F)),
-       auto.key = TRUE,
+       # scales=list(cex=.8, col="black", x = list(log = T), y = list(log = F)),
        par.settings = list(superpose.line = list(col = c("blue","red"),
                                                  lwd = 1)),
+       auto.key=list(space="top", columns=2, 
+                     title="Running profits from wagers 3 (blue) and 4 (red)", 
+                     cex.title=2,
+                     lines=TRUE, points=FALSE),
        xlab="Tries",
        xlim = c(1, n),
+       scales=list(cex=c(1.1, 1.1), # increase font size
+                   x = list(log = T), 
+                   y = list(log = F),
+                   alternating = 1,   # axes labels left/bottom 
+                   tck = c(1,0)),   # ticks only with labels
        ylab="Profit",
        ylim = c(-3.1, 3.1),
-       main="Running profits from wagers 3 (blue) and 4 (red)",
        type=c("l"),
-       panel = function( x,y,...) {
-         panel.abline( h=y[ which(y==0.0) ], lty = "dotted", col = "black", lwd = 1)
-         panel.xyplot( x,y,...)
+       # main = "Running profits from wagers 3 (blue) and 4 (red)",
+       panel=panel.superpose,
+       panel.groups = function( x,y,group.number,...) {
+         # print(levels(factor(df$wager))[group.number])
+         panel.abline( h=y[ which(y==0.0) ], lty = "dotted", col = "black")
+         panel.grid(v=-1, h=-1, lty=3)
+         xt <- x[x==log(min(x)+1)] # find latest year
+         yt <- y[x==min(x)] # find value at latest year
+         print(xt)
+         print(yt)
+         panel.text(xt, yt, labels=wagerLabels[group.number], 
+                    pos=4,  # show labels on right side
+                    ...)
+         panel.xyplot(x,y,...)
+         # print(levels(df$wager)[group.number])
+         # print(group.number[1])
        }
 )
 
