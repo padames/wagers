b8ae53294d74e021b4abb4f63a8eab674d4024d5
Pablo Adames <pablo.adames@gmail.com>
2021-09-06 14:15:46 -0600

    scripts for sim budget working

--- comparison_cashflow_dev.R
+++ comparison_cashflow_dev.R
@@ -1,41 +1,53 @@
 library(tigerstats)
 
 n = 2000
-wagerLabels = c("Wager 3", "Wager 4")
+wagerLabels = c("Wager3 = 300(die>3) - 300(die<=3)", "Wager 4 = 150(die>2) - 300(die<=2)")
 outsp = seq(1,6)
 die3 = sample(outsp,n,replace=T)
 die4 = sample(outsp,n,replace=T)
-cashflow3 = 300*(die3>3) - 300*(die3<=3)
-cashflow4 = 150*(die4>2) - 300*(die4<=2)
+cashflow3 = 300 - 600*(die3>3)
+cashflow4 = 300 - 450*(die4>2)
 
-cash3 = cumsum(cashflow3)
-cash4 = cumsum(cashflow4)
+cashflow3[1] <- 300
+cashflow3[1] <- 300
+cash3 = sapply(seq_along(1:n), function(i){ifelse(i==1, 300, sum(cashflow3[1:i]))})
+cash4 = sapply(seq_along(1:n), function(i){ifelse(i==1, 300, sum(cashflow4[1:i]))})
 
 # put together a data frame in long form
-df3 <- data.frame(wager=rep("wager 3",length(cash3)),cashflow=cash3,tries=seq(1,n))
-df4 <- data.frame(wager=rep("wager 4",length(cash4)),cashflow=cash4,tries=seq(1,n))
+# first the wage 4 so the lattice barchart format c(1,2) puts the wager 3 last or on top
+df3 <- data.frame(wager=rep(wagerLabels[1],length(cash3)),
+                  cashflow=cash3,
+                  tries=seq(1,n))
+df4 <- data.frame(wager=rep(wagerLabels[2],length(cash4)),
+                  cashflow=cash4,
+                  tries=seq(1,n))
 df <- rbind(df3, df4)
 
+# print(head(df))
+
 png(file="simbudget%02d.png", width=1200, height=800)
-colf <- function(w) { ifelse(test = w == "wager 3", 
-                             yes = "red", no = "blue")}
+
 my.settings <- list(
-  # superpose.polygon = sapply(df$wager, function(x){ifelse( x == "wager 3", "red", "blue")}), 
+  superpose.polygon=list(col=c("red","blue"), border="transparent"),
+  # plot.polygon = list(col=c("red")),
   strip.background = list(col="lightyellow"),
   strip.border = list(col= "black")
   )
 # print(trellis.par.get())
-pp <- barchart(cashflow~tries | wager, data=df, horiz=F,
+pp <- barchart(cashflow~tries, data=df, horiz=F,
                groups = wager,
-               box.ratio = 2,
-               lty=0.1, stack=F, 
+               # main="Cash needed to play",
+               box.ratio = 5,
+               aspect = "fill",
+               lty=0, stack=F, 
                origin=0, reference=T,
                par.settings = my.settings,
-               # auto.key = list(#space = "top", columns=2, 
-               #                 title="Cash flows for two wagers",
-               #                 cex.title=1.1),
+               # layout = c(1,2),
+               auto.key = list(space = "top", columns=2,
+                               title="Cash needed to play",
+                               cex.title=1.1),
                col=c("red","blue"),
-               ylim = c(-40000,40000),
+               ylim = c(-30000,30000),
                scales=list(
                  x=list(draw=T,
                         at=c(0,500,1000,1500,2000),
@@ -43,14 +55,15 @@ pp <- barchart(cashflow~tries | wager, data=df, horiz=F,
                         tck=(1)
                         ),
                  y=list(draw=T,
-                        at=c(-40000,-30000,-20000,-10000,-5000,0,5000,10000,20000,30000,40000),
-                        labels=c("-40,000", "-30,000","-20,000", "-10,000", "-5,000", "0", "5,000", "10,000", "20,000", "30,000","40,000"),
+                        at=c(-30000,-25000,-20000,-15000,-10000,-5000,0,5000,10000,15000,20000,25000,30000),
+                        labels=c("-30,000","-25,000", "-20,000","-15,000", "-10,000", "-5,000", "0", "5,000", "10,000", "15,000", "20,000","25,000", "30,000"),
                         tck=(1))
                ),
                # panel.groups = function( x,y, group.number,...) {
                #   xt <- x[y==min(y)] # find latest year
                #   yt <- y[y==min(y)] # find value at latest year
-               #   panel.text(xt, yt, labels=wagerLabels[group.number], 
+               #   print(group.number)
+               #   panel.text(xt, yt, labels=wagerLabels[group.number],
                #              pos=4,  # show labels on right side
                #              ...)
                #   panel.barchart(x,y,stack=F,lty = 0.1,col = ifelse(group.number==1 , "red", "blue"),border = F, box.width=0.5,...)
---
+++ wager_budget_visualization.R
@@ -0,0 +1,85 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("here")
+
+suppressWarnings(my_proj_path <- here())
+
+if (!file.exists(file.path(my_proj_path,"animations"))) {
+  dir.create(file.path(my_proj_path, "animations"))
+}
+setwd(file.path(my_proj_path, "animations"))
+
+library(lattice)
+library(tigerstats)
+
+my.settings <- list(
+  superpose.polygon=list(col=c("red","blue"), border="transparent")
+)
+
+
+n = 2000
+
+png(file="simbudget%02d.png", width=1200, height=800)
+
+wagerLabels = c("Wager3 = 300(die>3) - 300(die<=3)", "Wager 4 = 150(die>2) - 300(die<=2)")
+
+outsp = seq(1,6)
+
+for (i in 1:60) {
+  
+  die3 = sample(outsp,n,replace=T)
+  die4 = sample(outsp,n,replace=T)
+  cashflow3 = 300 - 600*(die3>3)
+  cashflow4 = 300 - 450*(die4>2)
+  
+  # money necessary for first wager entry:
+  cashflow3[1] <- 300
+  cashflow3[1] <- 300
+  
+  cash3 = sapply(seq_along(1:n), function(i){ifelse(i==1, 300, sum(cashflow3[1:i]))})
+  cash4 = sapply(seq_along(1:n), function(i){ifelse(i==1, 300, sum(cashflow4[1:i]))})
+  
+  df3 <- data.frame(wager=rep(wagerLabels[1],length(cash3)),
+                    cashflow=cash3,
+                    tries=seq(1,n))
+  df4 <- data.frame(wager=rep(wagerLabels[2],length(cash4)),
+                    cashflow=cash4,
+                    tries=seq(1,n))
+  df <- rbind(df3, df4)
+
+  pp <- barchart(cashflow~tries, data=df, horiz=F,
+                 groups = wager,
+                 box.ratio = 5,
+                 aspect = "fill",
+                 lty=0, stack=F, 
+                 origin=0, reference=T,
+                 par.settings = my.settings,
+                 auto.key = list(space = "top", columns=2,
+                                 title="Cash needed to play",
+                                 cex.title=1.1),
+                 col=c("red","blue"),
+                 ylim = c(-35000,35000),
+                 scales=list(
+                   x=list(draw=T,
+                          at=c(0,500,1000,1500,2000),
+                          labels=c("0","500","1,000","1,500","2,000"),
+                          tck=(1)
+                   ),
+                   y=list(draw=T,
+                          at=c(-35000,-30000,-25000,-20000,-15000,-10000,-5000,0,5000,10000,15000,20000,25000,30000,35000),
+                          labels=c("-35,000","-30,000","-25,000", "-20,000","-15,000", "-10,000", "-5,000", "0", "5,000", "10,000", "15,000", "20,000","25,000", "30,000","35,000"),
+                          tck=(1))
+                 )
+  )
+  print(pp)  
+}
+
+dev.off()
+
+# This section is machine dependent. 
+# It assumes we are in an Ubuntu installation with the program convert from ImageMagick
+
+# converting .png file in .gif using ImageMagick
+system("/usr/bin/convert -delay 40 *.png money_to_play_by_wager.gif")
+
+# Remove .png file
+file.remove(list.files(pattern=".png"))
